e80e0fe6f32128e08ebebe137b233523ca1d412d
Hua Zhang <hua.zhang@wsu.edu>
2016-07-29 23:50:23 -0700

    data structure and algorithm in Swift

---
+++ swift-algo/swift-algo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-algo
+//
+//  Created by hua zhang on 7/29/16.
+//  Copyright © 2016 hua zhang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
