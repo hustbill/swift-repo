32e365053b51dba463646f0a56fc8a3ca8c14b2c
Bill Zhang <billyzhang2010@gmail.com>
2016-08-17 17:59:07 -0700

    calibration/BLEScanViewController.swift

---
+++ calibration/calibration/BLEScanViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BLEScanViewController.swift
+//  calibration
+//
+//  Created by bill on 8/17/16.
+//  Copyright © 2016 bill. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class BLEScanViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("BLEScanViewController")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Actions
+    
+  
+}
