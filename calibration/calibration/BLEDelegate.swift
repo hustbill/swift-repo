ea679fe8a3a08795fccd291227a09fb8032a4a37
Bill Zhang <billyzhang2010@gmail.com>
2016-08-19 17:46:22 -0700

    BLEFatory code

---
+++ calibration/calibration/BLEDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  BLEDelegate.swift
+//  calibration
+//
+//  Created by bill on 8/19/16.
+//  Copyright © 2016 bill. All rights reserved.
+//
+
+import Foundation
---
+++ calibration/calibration/BLEFactory.swift
@@ -0,0 +1,9 @@
+//
+//  BLEFactory.swift
+//  calibration
+//
+//  Created by bill on 8/19/16.
+//  Copyright © 2016 bill. All rights reserved.
+//
+
+import Foundation
